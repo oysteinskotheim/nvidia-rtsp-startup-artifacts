a22073dd4738cb9bb98d0e9e855ef1db7f422547
Øystein Skotheim <oystein.skotheim@scoutdi.com>
2021-12-20 15:38:52 +0100

    Add proper handling of SIGINT and SIGTERM

--- src/rtsp_server.cpp
+++ src/rtsp_server.cpp
@@ -1,8 +1,28 @@
 #include "NvArgusCamera.h"
+#include <glib-unix.h>
+#include <glib.h>
 #include <iostream>
 #include <signal.h>
 
 GMainLoop *mainloop;
+
+static gboolean exit_signal_handler(gpointer mainloop) {
+  g_main_loop_quit((GMainLoop *)mainloop);
+  return true;
+}
+
+static void setup_signal_handlers() {
+  GSource *source = NULL;
+  GMainContext *ctx = g_main_loop_get_context(mainloop);
+
+  source = g_unix_signal_source_new(SIGINT);
+  g_source_set_callback(source, exit_signal_handler, mainloop, NULL);
+  g_source_attach(source, ctx);
+
+  source = g_unix_signal_source_new(SIGTERM);
+  g_source_set_callback(source, exit_signal_handler, mainloop, NULL);
+  g_source_attach(source, ctx);
+}
 
 void signal_handler(int signal) { g_main_loop_quit(mainloop); }
 
@@ -12,8 +32,7 @@ int main(int argc, char *argv[]) {
 
     mainloop = g_main_loop_new(NULL, FALSE);
 
-    signal(SIGINT, signal_handler);
-    signal(SIGTERM, signal_handler);
+    setup_signal_handlers();
 
     NvArgusCamera camera;
     camera.startStreaming();
